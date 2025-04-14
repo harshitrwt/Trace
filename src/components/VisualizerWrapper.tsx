66bcc002fdb60cb45e298d2bd59cd5fa28817e5e
itsharshitrwt <harshit.rawat1804@gmail.com>
2025-04-14 13:05:28 +0530

    fixes1

--- src/components/VisualizerWrapper.tsx
+++ src/components/VisualizerWrapper.tsx
@@ -111,8 +111,8 @@ import AIExplanationPanel from './AiPanel';
             </nav>
   
             {/* Small screen InputArea (above Canvas) */}
-            <div className="grid grid-cols-1 lg:grid-cols-3">
-              <div className="lg:col-span-2 space-y-6 ">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+              <div className="lg:col-span-2 space-y-6">
                 <AlgorithmSelector />
   
                 <div className="lg:hidden mb-6">
